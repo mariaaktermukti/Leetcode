0385200be6f19c60d8437505bd58cd009c709cab
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-03-12 21:52:39 +0600

    Add files via upload

---
+++ happy_number.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int getNext(int n) {
+    int sum = 0;
+    while (n > 0) {
+        int digit = n % 10;
+        sum += digit * digit;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool isHappy(int n) {
+    int slow = n, fast = getNext(n);
+    while (fast != 1 && slow != fast) {
+        slow = getNext(slow);
+        fast = getNext(getNext(fast));
+    }
+    return fast == 1;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (isHappy(n)) {
+        cout << n << " is a Happy Number." << endl;
+    } else {
+        cout << n << " is NOT a Happy Number." << endl;
+    }
+
+    return 0;
+}
