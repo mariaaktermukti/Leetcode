156b04f4f709d8c7f22168455b61dcc6b4b305c2
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-03-25 11:19:43 +0600

    Add files via upload

---
+++ binary_tree_Zigzag_level_order_traversal.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true;
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level(size);
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                int index = leftToRight ? i : (size - 1 - i);
+                level[index] = node->val;
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            result.push_back(level);
+            leftToRight = !leftToRight; 
+        }
+
+        return result;
+    }
+};
+
+TreeNode* insertLevelOrder(vector<int> arr, int i) {
+    if (i >= arr.size() || arr[i] == -1) return nullptr;
+    TreeNode* root = new TreeNode(arr[i]);
+    root->left = insertLevelOrder(arr, 2 * i + 1);
+    root->right = insertLevelOrder(arr, 2 * i + 2);
+    return root;
+}
+
+int main() {
+    vector<int> nodes = {3, 9, 20, -1, -1, 15, 7};
+    TreeNode* root = insertLevelOrder(nodes, 0);
+
+    Solution sol;
+    vector<vector<int>> result = sol.zigzagLevelOrder(root);
+
+    cout << "[";
+    for (int i = 0; i < result.size(); i++) {
+        cout << "[";
+        for (int j = 0; j < result[i].size(); j++) {
+            cout << result[i][j];
+            if (j < result[i].size() - 1) cout << ",";
+        }
+        cout << "]";
+        if (i < result.size() - 1) cout << ",";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
