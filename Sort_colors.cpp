b2f47d318115c53f857e5af5a9da141599125da2
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-04-11 12:06:34 +0600

    Add files via upload

---
+++ Sort_colors.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size() - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements (0, 1, or 2): ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    Solution sol;
+    sol.sortColors(nums);
+
+    cout << "Sorted colors: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
