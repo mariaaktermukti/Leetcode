460567742fa6a8e7e7172b76389ebbb2b0ebe68a
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2026-03-05 22:13:22 +0600

    Add files via upload

---
+++ Small_Palindrome.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int x, y;
+        cin >> x >> y;
+        vector < long > v(x + y);  
+        int i = 0, j = x + y - 1; 
+        while (x > 0) 
+        {
+            v[i] = 1;
+            v[j] = 1;
+            i++;
+            j--;
+            x -= 2;
+        }
+        while (y > 0) 
+        {
+            v[i] = 2;
+            v[j] = 2;
+            i++;
+            j--;
+            y -= 2;
+        }
+        long ans = 0;
+        for (int i = 0; i < v.size(); i++) 
+        {
+            ans = ans * 10 + v[i];
+        }
+        cout << ans << endl;
+    }
+}
